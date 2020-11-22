4e80fed0041ad6f7f915d2ad4a4287c073e92fbb
Andrea Giove <andreagiove@outlook.com>
2020-11-22 10:43:58 +0100

    alpha version of the action

--- sdk/webhooks/ActionsOnGoogleFulfillment/index.js
+++ sdk/webhooks/ActionsOnGoogleFulfillment/index.js
@@ -1,4 +1,4 @@
-const { conversation } = require('@assistant/conversation');
+const { conversation, Card } = require('@assistant/conversation');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
@@ -14,21 +14,61 @@ function getMenu(menuDoc, dateStr, meal) {
   return menuDoc.data().menu[dateStr][meal];
 }
 
+function getWhenOrDefault(when) {
+  let queryDate;
+  if (when) {
+    queryDate = new Date(when.year, when.month - 1, when.day);
+  } else {
+    const now = new Date();
+    queryDate = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  }
+  queryDate.setMinutes(queryDate.getMinutes() - queryDate.getTimezoneOffset());
+  return queryDate;
+}
+
+function getMealOrDefault(meal, queryDate) {
+  if (meal) return meal;
+  
+  const now = new Date();
+  const isTodayQuery = now.getFullYear() == queryDate.getFullYear() &&
+                       now.getMonth() == queryDate.getMonth() &&
+                       now.getDate() == queryDate.getDate();
+  console.log("meal debug", JSON.stringify({isTodayQuery, nowHours: now.getUTCHours()}));
+  return (isTodayQuery && now.getUTCHours() > 14) ? 'dinner' : 'launch';
+}
+
+function getSpeakOutResponse(menu, canteen, meal, queryDate) {
+  const canteenUpper = canteen.charAt(0).toUpperCase() + canteen.slice(1);
+  const mealOut = (meal == 'launch') ? 'pranzo' : 'cena';
+  return `A ${mealOut} alla ${canteenUpper} c'è: ${menu}.`;
+}
+
+function getCardResponse(menu, canteen, meal, queryDate) {
+  const canteenUpper = canteen.charAt(0).toUpperCase() + canteen.slice(1);
+  const mealOut = (meal == 'launch') ? 'pranzo' : 'cena';
+  const dateStr = queryDate.getDate() + "/" + (queryDate.getMonth() + 1);
+  return new Card({
+    title: canteenUpper,
+    subtitle: dateStr + '-' + mealOut,
+    text: menu
+  });
+}
+
 app.handle('get_menu', async conv => {
   const { canteen, when, meal } = conv.session.params;
-  let queryDate = (when) ? new Date(when.year, when.month - 1, when.day) : new Date();
-  queryDate.setMinutes(queryDate.getMinutes() - queryDate.getTimezoneOffset());
-  const queryMeal = (meal) ? meal : 'launch';
+  const queryDate = getWhenOrDefault(when);
+  const queryMeal = getMealOrDefault(meal, queryDate);
   
   const menuDoc = await db.collection('menu').doc(canteen).get();
   const menu = getMenu(menuDoc, queryDate.toISOString(), queryMeal);
+
   if (!menu) {
     conv.add(`Il menu della ${canteen} non è disponibile.`);
   } else {
-    conv.add(`Alla ${canteen} c'è: ${menu}`);
+    conv.add(getSpeakOutResponse(menu, canteen, meal, queryDate));
+    conv.add(getCardResponse(menu, canteen, meal, queryDate));
   }
-  conv.scene.next.name = 'actions.scene.END_CONVERSATION';
-  
+  conv.scene.next.name = 'PreEnd';
 });
 
 exports.ActionsOnGoogleFulfillment = functions.https.onRequest(app);
